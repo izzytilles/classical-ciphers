e12c1cf6a0b915052bd475680a40fd5de57ece46
zerpelding <zerpelding@zagmail.gonzaga.edu>
2024-08-31 18:09:57 -0700

    will now return none for bad input

--- Group1_CPSC353_CipherCode.py
+++ Group1_CPSC353_CipherCode.py
@@ -4,42 +4,23 @@ One Time Pad uses a key that is the same exact length as the message.
 
 Input: the message and key containing only letters and spaces (will fail gracefully is given anything else)
 Output: the encrypted message and the key (with some words labeling each)
+        If there is an issue, it will output None
 
 """
-def oneTimePadEncr(message_input, key_input):
+def one_time_pad_encr(message_input, key_input):
     
     # Frist we make sure that the lengths of the message and key match
     length = len(message_input)     # We will use this later so it gets its own variable
     if length != len(key_input):
-        return "There was an issue. The length of the key is not the same as the length of the encrypted message. Please try again."
-
+        return None
     
     # We want to make sure the message and key are in capital letters and do not contain special characters
-    message = []            # This is the version of the message in capital letters
-    key = []                # This is the version of the key in capital letters
-    for k in range(length):
-        # This section checks the message and fills the message list with the proper letters in upper case
-        if message_input[k] == " ":                                             # If the message is a space
-            message.append(" ")
-        elif ord(message_input[k]) > 96 and ord(message_input[k]) < 123:         # If the message is lower case 
-            message.append(message_input[k].upper())
-        elif ord(message_input[k]) > 64 and ord(message_input[k]) < 91:         # If the message is in upper case
-            message.append(message_input[k])
-        else:
-            print("An unacceptable character was included in the message")      
-            return "Please try again only using letters and spaces"
-        
-        # This section checks the key and fills the key list with the proper letters in upper case
-        if key_input[k] == " ":                                         # If the key is a space
-            key.append(" ")
-        elif ord(key_input[k]) > 96 and ord(key_input[k]) < 123:         # If the key is lower case
-            key.append(key_input[k].upper())
-        elif ord(key_input[k]) > 64 and ord(key_input[k]) < 91:         # If the key is in upper case
-            key.append(key_input[k])
-        else:
-            print("An unacceptable character was included in the key")      
-            return "Please try again only using letters and spaces"
-
+    message = convertToList(message_input)            # This is the version of the message in capital letters
+    if message == None:
+        return None
+    key = convertToList(key_input)                # This is the version of the key in capital letters
+    if key == None:
+        return None
 
     encr = []                               # This is the encrypted message that we will output
 
@@ -66,9 +47,8 @@ def oneTimePadEncr(message_input, key_input):
     final_encrypt = "".join(encr)
     final_key = "".join(key)
 
-    return  "Encrypted Message is: "+ final_encrypt + "     and     Key is: " + final_key
+    return  [final_encrypt, final_key]
 
-# ^^^ Above it returns a whole statement with labels for the encryption message and key. If the testing makes more sense for it to say something else feel free to change. 
 
 """ 
 This is the One Time Pad Decryption Function. 
@@ -76,29 +56,31 @@ One Time Pad uses a key that is the same exact length as the message.
 
 Input: the encrypted message and key containing only letters and spaces (will fail gracefully is given anything else)
 Output: the decrypted message
+        If there is an issue it will return None
 
 """
-def oneTimePadDecr(encr_mess, key):
-    # Change the input encryption message into a list of characters
-    encr = []
-    encr[:0] = encr_mess
+def one_time_pad_decr(encr_mess, key_in):
+    # Change the input encryption message into a list of characters. Will capitalize the lower case letters.
+    encr = convertToList(encr_mess)
+    if encr == None:
+        return None
     
+    # Change the input key into a list of characters. Will capitalize the lower case letters.
+    key = convertToList(key_in)
+    if key == None:
+        return None
+
     # initialize our descryption list
     decr = []
     
     # make sure that the length of the message and the key match
     length = len(encr_mess)
-    if length != len(key):
-        return "There was an issue. The length of the key is not the same as the length of the encrypted message. Please try again."
+    if length != len(key_in):
+        return None
 
-    # This for loop 
+    # This for loop decrypts the message
     for i in range(length):                 
-        # make sure the key is right
-        if key[i] == " " or ( ord(key[i]) > 64 and ord(key[i]) < 91):
-            pass
-        else:
-            return "There was an issue. Your key contains an unacceptable character. It should only be comprised of spaces and capital letters"
-
+        
         # If the "letter" of the encrypted message is a space
         if encr[i] == " ":
             letterDecr = (26 - ord(key[i]) + 65) % 27 + 65      # The space value starts at 26, we subtract the value of the key which is the ord()-65 (double negative makes a positive so we add 65), mod 27 to bring it back to our range of 0-26, then add 65 to bring it back to ASCII
@@ -115,21 +97,33 @@ def oneTimePadDecr(encr_mess, key):
             else:
                 decr.append(chr(letterDecr))
         
-        # If the "letter" doesn't fit the specifications
-        else:
-            return "There was an issue. Your encrypted message contains an unacceptable character. It should only be comprised of spaces and capital letters."
-
     return "".join(decr)        # This is the pretty string version of the decr list
 
 
-
-
+"""
+Converts a string to a list
+Input: a string
+Output: the list of capitalized characters
+        If the input has an unacceptable character, it will return None
+"""
+def convertToList(string):
+    message = []
+    for k in range(len(string)):
+        if string[k] == " ":                                             # If the message is a space
+            message.append(" ")
+        elif ord(string[k]) > 96 and ord(string[k]) < 123:         # If the message is lower case 
+            message.append(string[k].upper())
+        elif ord(string[k]) > 64 and ord(string[k]) < 91:         # If the message is in upper case
+            message.append(string[k])
+        else:
+            return None
+    return message
 
 # My personal testing. Feel free to delete or change. 
 message = input("Write message: ")
 key = input("Write key of the same length as the message: ")
-print(oneTimePadEncr(message, key))
+print(one_time_pad_encr(message, key))
 
 decryption = input("Encrypted Message: ")
 key = input("Key: ")
-print("The message is: " + oneTimePadDecr(decryption, key))
+print("The message is: " + one_time_pad_decr(decryption, key))
