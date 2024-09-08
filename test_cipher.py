b64eb35349f54718fbe6c37e106071fba632c745
izzytilles <123509336+izzytilles@users.noreply.github.com>
2024-09-08 16:35:53 -0700

    added comments for every test, and changed some tests a little - currently all of them pass

--- test_cipher.py
+++ test_cipher.py
@@ -5,8 +5,7 @@ from one_time_pad import one_time_pad_decr, one_time_pad_encr
 from caesar import caesar_enc, caesar_dec
 from vigenere import vigenere_encr, vigenere_decrypt
 
-# test one time pad
-
+# test to ensure that a key that is too long will not be allowed for one time pad encryption
 def test_one_time_pad_encr_long_key():
     encr_message = "HI"
     key = "DOG"
@@ -15,6 +14,7 @@ def test_one_time_pad_encr_long_key():
 
     assert result == None
 
+# test to ensure that a key that is too short will not be allowed for one time pad encryption
 def test_one_time_pad_encr_short_key():
     encr_message = "HI"
     key = "D"
@@ -23,6 +23,7 @@ def test_one_time_pad_encr_short_key():
 
     assert result == None
 
+# test to ensure that a key that is too long will not be allowed for one time pad decyrption
 def test_one_time_pad_decr_long_key():
     decr_message = "HI"
     key = "DOG"
@@ -31,6 +32,7 @@ def test_one_time_pad_decr_long_key():
 
     assert result == None
 
+# test to ensure that a key that is too short will not be allowed for one time pad decryption
 def test_one_time_pad_decr_short_key():
     decr_message = "HI"
     key = "D"
@@ -39,6 +41,7 @@ def test_one_time_pad_decr_short_key():
 
     assert result == None
 
+# test to ensure that an attempt with no key will not be allowed for one time pad encyption
 def test_one_time_pad_encr_no_key():
     encr_message = "IZZY"
     key = ""
@@ -47,6 +50,7 @@ def test_one_time_pad_encr_no_key():
 
     assert result == None
 
+# test to ensure that an attempt with no message will not be allowed for one time pad encyption
 def test_one_time_pad_encr_no_message():
     encr_message = ""
     key = "HELLO"
@@ -55,6 +59,7 @@ def test_one_time_pad_encr_no_message():
 
     assert result == None
 
+# test to ensure that one time pad encryption works
 def test_one_time_pad_encr():
     encr_message = "HELLO"
     key = "MEDIA"
@@ -63,6 +68,7 @@ def test_one_time_pad_encr():
 
     assert result == ["TIOTO", "MEDIA"] # returns a tuple
 
+# test to ensure that one time pad encyption will not run with lower case input
 def test_one_time_pad_encr_lower():
     encr_message = "hello"
     key = "media"
@@ -71,6 +77,7 @@ def test_one_time_pad_encr_lower():
 
     assert result == None
 
+# test to ensure that one time pad decryption works
 def test_one_time_pad_decr():
     decr_message = "TIOTO"
     key = "MEDIA"
@@ -79,69 +86,70 @@ def test_one_time_pad_decr():
 
     assert result == "HELLO"
 
+# test to ensure that one time pad decryption will not run with lower case input
 def test_one_time_pad_decr_lower():
     decr_message = "tioto"
     key = "media"
 
-    result = one_time_pad_decr(decr_message, key)
+    result = one_time_pad_decr(convert_to_list(decr_message), key)
 
     assert result == None
 
-# test caesar
-
+# test to ensure that caesar encryption works
 def test_caesar_encr():
-    encr_message = "A"
+    encr_message = "AWAY WE GO"
     key = "X"
 
     result = caesar_enc(encr_message, key)
 
-    assert result == "X"
+    assert result == "XSXUWSAWCK"
 
+# test to ensure that caesar encryption will not work with lowercase input 
 def test_caesar_encr_lower():
-    encr_message = "a"
+    encr_message = "away we go"
     key = "x"
-
-    result = caesar_enc(encr_message, key)
-
-    assert result == "X"
-
-def test_caesar_decr():
-    encr_message = "X"
-    key = "A"
-
-    result = caesar_enc(encr_message, key)
-
-    assert result == "X"
-
-def test_caesar_encr_lower():
-    encr_message = "x"
-    key = "a"
 
     result = caesar_enc(encr_message, key)
 
     assert result == None
 
+# test to ensure that caesar decryption works
+def test_caesar_decr():
+    encr_message = "XSXUWSAWCK"
+    key = "X"
+
+    result = caesar_dec(encr_message, key)
+
+    assert result == "AWAY WE GO"
+
+# test to ensure that caesar decryption does not work with lowercase input
+def test_caesar_dec_lower():
+    encr_message = "xsxuwsawck"
+    key = "a"
+
+    result = caesar_dec(encr_message, key)
+
+    assert result == None
+
+# test to ensure that caesar encryption doesn't work with a key that's longer that one character
 def test_caesar_encr_long_key():
-    encr_message = "X"
+    encr_message = "HELLO"
     key = "AB"
 
     result = caesar_enc(encr_message, key)
 
     assert result == None
 
+# test to ensure that caesar decryption doesn't work with a key that's longer that one character
 def test_caesar_decr_long_key():
-    decr_message = "X"
+    decr_message = "HELLO"
     key = "AB"
 
     result = caesar_dec(decr_message, key)
 
     assert result == None
 
-# def test_space_handler():
-
-
-# test vigenere
-
+# test to ensure that vigenere encryption works
 def test_vigenere_encr():
     encr_message = "HELLO"
     key = "CAT"
@@ -150,14 +158,16 @@ def test_vigenere_encr():
 
     assert result == "JEDNO" 
 
+# test to ensure that vigenere encryption doesn't work with lowercase input
 def test_vigenere_encr_lower():
     encr_message = "hello"
     key = "cat"
 
     result = vigenere_encr(encr_message, key)
 
-    assert result == None 
+    assert result == None
 
+# test to ensure that vigenere decryption works
 def test_vigenere_decr():
     decr_message = "JEDNO"
     key = "CAT"
@@ -166,14 +176,7 @@ def test_vigenere_decr():
 
     assert result == "HELLO"
 
-def test_vigenere_decr_lower():
-    decr_message = "jedno"
-    key = "cat"
-
-    result = vigenere_decrypt(decr_message, key)
-
-    assert result == "HELLO"
-
+# test to ensure that vigenere encryption doesn't work if the key is longer than the word to be encrypted
 def test_vigenere_encr_long_key():
     encr_message = "cat"
     key = "hello"
@@ -182,6 +185,7 @@ def test_vigenere_encr_long_key():
 
     assert result == None 
 
+# test to ensure that vigenere decryption doesn't work if the key is longer than the word to be decrypted
 def test_vigenere_decr_long_key():
     decr_message = "cat"
     key = "hello"
